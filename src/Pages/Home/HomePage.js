27a1e47d2999a27bcce1a4afd0625ba8265fc1ae
DevAkky <114848776+devakashpandey@users.noreply.github.com>
2023-10-21 02:24:06 +0530

    Folder structure done

---
+++ src/Pages/Home/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../../components/Navbar/Nav";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>
+        <Nav />
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/components/Navbar/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <nav>NAV BAR</nav>
+    </>
+  );
+};
+
+export default Nav;
